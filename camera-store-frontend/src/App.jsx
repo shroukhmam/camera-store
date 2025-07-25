518631822741ec1354bf138d119721b8dc68de35
shroukHmam200 <shrouk.hmam@edigits.net>
2025-07-25 22:16:15 +0300

    compontents

--- camera-store-frontend/src/App.jsx
+++ camera-store-frontend/src/App.jsx
@@ -1,10 +1,16 @@
-
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./Pages/Login.jsx";
 
 function App() {
-
-  return (
-    <div className='text-red-500'>jjjjj</div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
---
+++ camera-store-frontend/src/Pages/Login.jsx
@@ -0,0 +1,10 @@
+
+
+function Login() {
+
+    return (
+        <p className="text-3xl font-bold text-green-500">Hello Tailwind</p>
+    )
+}
+
+export default Login
