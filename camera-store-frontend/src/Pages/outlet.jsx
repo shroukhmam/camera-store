ca2a98735e2c22a8ec245ef534bbdbd6631b7d61
shroukHmam200 <shrouk.hmam@edigits.net>
2025-07-28 22:21:57 +0300

    shrouk

--- camera-store-frontend/src/Pages/outlet.jsx
+++ camera-store-frontend/src/Pages/outlet.jsx
@@ -2,29 +2,20 @@ import ProductCarousel from "../components/ProductCarousel.jsx";
 import categoriesData from "../data/CategoryProduct.json";
 import OutletDetails from "../components/outletDetails.jsx";
 
-export default function Outlet({ onAddToCart }) {
-    if (!categoriesData?.categories) {
-        return <div>Loading products...</div>;
-    }
-
-
-    const lightsCategory = categoriesData.categories.find(
-        category => category.name.toLowerCase() === 'lights'
-    );
-
-    const lightProducts = lightsCategory?.products || [];
-    const stabilizerCategory = categoriesData.categories.find(
-        category => category.name.toLowerCase() === 'stabilizer'
-    );
-
-    const stabilizerProducts = stabilizerCategory?.products || [];
+export default function Outlet() {
 
     return (
         <>
             <OutletDetails />
-            <ProductCarousel products={lightProducts} nameSection="Lights......" onAddToCart={onAddToCart} />
-            <ProductCarousel products={stabilizerProducts} nameSection="Stabilizer......" onAddToCart={onAddToCart} />
-
+            {categoriesData.categories
+                .filter(category => category.products && category.products.length > 0)
+                .map(category => (
+                    <ProductCarousel
+                        products={category.products}
+                        nameSection={`${category.name}......`}
+                    />
+                ))
+            }
         </>
     );
 }
